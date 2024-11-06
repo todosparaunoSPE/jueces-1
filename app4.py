5a7cf50888185195ac0df61c08a3c22c689b7f80
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2024-11-06 14:41:27 -0600

    Update app4.py

--- app4.py
+++ app4.py
@@ -8,16 +8,38 @@ import streamlit as st
 import PyPDF2
 import pandas as pd
 import spacy
-import os
+
 import numpy as np
 from nlp_model import load_spacy_model  # Importa la función desde nlp_model.py
 
-# Verifica si el modelo ya está descargado, si no, lo descarga
-if not os.path.exists(spacy.util.get_package_path("en_core_web_sm")):
-    os.system("python -m spacy download en_core_web_sm")
+import os
+import subprocess
+
+# Verifica si el modelo ya está descargado, si no lo descarga
+def download_spacy_model():
+    try:
+        # Intentar cargar el modelo
+        spacy.load("en_core_web_sm")
+    except OSError:
+        # Si ocurre un error, significa que el modelo no está descargado
+        st.write("El modelo 'en_core_web_sm' no está descargado. Procediendo a la instalación...")
+        
+        # Ejecutar el comando para descargar el modelo
+        subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"], check=True)
+        st.write("Modelo descargado con éxito.")
+        
+        # Intentar cargar el modelo nuevamente
+        spacy.load("en_core_web_sm")
+        st.write("Modelo cargado correctamente.")
+
+# Llamar a la función para asegurarse de que el modelo esté disponible
+download_spacy_model()
 
 # Cargar el modelo
 nlp = spacy.load("en_core_web_sm")
+
+
+
 
 # Función para extraer texto de archivos PDF
 def extract_text_from_pdf(pdf_file):
